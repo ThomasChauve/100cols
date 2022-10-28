ced489e042d2441e02fbc1fb02fd9da32c293b88
Chauve <thomaschauve.sci@disroot.org>
2022-10-29 01:31:31 +0200

    Update 1_📝_Liste_des_cols.py

--- pages/1_📝_Liste_des_cols.py
+++ pages/1_📝_Liste_des_cols.py
@@ -31,10 +31,11 @@ if len(user_list)!=0:
             st.download_button('Télécharger les données de '+ data.pseudo,pickle_model(data),file_name=data.pseudo+str(datetime.date.today())+'.100cols',key='uk-1'+str(k))
             k+=1
 
-st.title("Liste des cols de "+ option)
 if len(user_list)==0:
+    st.title("Liste des cols ")
     st.warning("Pas d'utilisateur enregistré")
 else:
+    st.title("Liste des cols de "+ option)
     if len(st.session_state['data_list'][st.session_state['id_u']].cols)==0:
         st.warning(st.session_state['data_list'][st.session_state['id_u']].name+" n'a pas encore de col enregistré.")
     else:
