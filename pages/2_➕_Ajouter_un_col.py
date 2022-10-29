46a09ccfb4d6a8cdc78179cd110c9999aa05384a
Chauve <thomaschauve.sci@disroot.org>
2022-10-29 08:06:00 +0200

    Update 2_➕_Ajouter_un_col.py

--- pages/2_➕_Ajouter_un_col.py
+++ pages/2_➕_Ajouter_un_col.py
@@ -41,48 +41,49 @@ else:
     st.title('Ajouter un col à '+option)
     file=os.listdir('database/basecol/data_website/')
     op_ly = st.multiselect('Librairie :',file,default='France.csv')
-    colAll_list=[]
-    for oo in op_ly:
-        colAll_list.append(lb100.list_biblio100cols(filename='database/basecol/data_website/'+oo).database)
-    
-    colsAll_tmp = pd.concat(colAll_list, axis=0, ignore_index=True)
-    
-    colAll=lb100.list_biblio100cols(filename='database/basecol/data_website/'+op_ly[0])
-    colAll.database=colsAll_tmp
-                  
-    st.header('Recherche')
+    if len(op_ly)!=0:
+        colAll_list=[]
+        for oo in op_ly:
+            colAll_list.append(lb100.list_biblio100cols(filename='database/basecol/data_website/'+oo).database)
 
-    f_id=st.text_input('Code',label_visibility="visible")
-    f_nom=st.text_input('Nom',label_visibility="visible")
-    f_alt=st.number_input('Altitude', label_visibility="visible",min_value=int(0))
+        colsAll_tmp = pd.concat(colAll_list, axis=0, ignore_index=True)
 
+        colAll=lb100.list_biblio100cols(filename='database/basecol/data_website/'+op_ly[0])
+        colAll.database=colsAll_tmp
 
-    if f_alt==0:
-        aa=None
-    else:
-        aa=f_alt
+        st.header('Recherche')
+
+        f_id=st.text_input('Code',label_visibility="visible")
+        f_nom=st.text_input('Nom',label_visibility="visible")
+        f_alt=st.number_input('Altitude', label_visibility="visible",min_value=int(0))
 
 
-    id=colAll.filter_name(f_nom,dep=f_id,alt=aa,print_res=False)
-    st.dataframe(colAll.database.loc[id])
-    
-    if np.sum(id)<5000:
-        with st.expander("Montrer sur la carte :"):
-            fig_map=colAll.plot_map(id,ww=width)
-            st.plotly_chart(fig_map, use_container_width=True, sharing="streamlit")
-
-    st.header('Ajouter')
-
-    f_date=st.date_input('Date de monté', label_visibility="visible")
-
-    add_code = st.selectbox('Col à ajouter',colAll.database.loc[id])
-
-    
-    if st.button('Ajouter'):
-        index=list(colAll.database[colAll.database.columns[0]]).index(add_code)
-        
-        txt=st.session_state['data_list'][st.session_state['id_u']].add_pass(colAll.database.loc[index],f_date)
-        if 'ajouter' in txt:
-            st.success(txt, icon="✅")
+        if f_alt==0:
+            aa=None
         else:
-            st.warning(txt, icon="⚠️")
+            aa=f_alt
+
+
+        id=colAll.filter_name(f_nom,dep=f_id,alt=aa,print_res=False)
+        st.dataframe(colAll.database.loc[id])
+
+        if np.sum(id)<5000:
+            with st.expander("Montrer sur la carte :"):
+                fig_map=colAll.plot_map(id,ww=width)
+                st.plotly_chart(fig_map, use_container_width=True, sharing="streamlit")
+
+        st.header('Ajouter')
+
+        f_date=st.date_input('Date de monté', label_visibility="visible")
+
+        add_code = st.selectbox('Col à ajouter',colAll.database.loc[id])
+
+
+        if st.button('Ajouter'):
+            index=list(colAll.database[colAll.database.columns[0]]).index(add_code)
+
+            txt=st.session_state['data_list'][st.session_state['id_u']].add_pass(colAll.database.loc[index],f_date)
+            if 'ajouter' in txt:
+                st.success(txt, icon="✅")
+            else:
+                st.warning(txt, icon="⚠️")
