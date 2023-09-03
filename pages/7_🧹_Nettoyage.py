5cb77311a47caf7a147de2205e8d5058b7b247c4
Chauve <thomaschauve.sci@disroot.org>
2023-09-03 13:59:16 +0000

    add cleaning page

---
+++ pages/7_🧹_Nettoyage.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import numpy as np
+import pickle
+import io
+import datetime
+
+st.set_page_config(page_title="Liste des cols", page_icon="📝")
+
+st.session_state['d_date']=datetime.date.today()
+st.session_state['filter_gpx']=False
+st.session_state['id_f']=None
+st.session_state['up_gpx']=None
+
+if 'id_u' not in st.session_state:
+    st.session_state['id_u'] = 0
+
+if 'data_list' not in st.session_state:
+    st.session_state['data_list']=[]
+
+user_list=[]
+for du in st.session_state['data_list']:
+    user_list.append(du.pseudo)
+
+def pickle_model(model):
+    f = io.BytesIO()
+    pickle.dump(model.__dict__, f)
+    return f 
+
+if len(user_list)!=0:
+    with st.sidebar:
+        option = st.selectbox('Utilisateur',user_list,index=st.session_state['id_u'],key='u1')
+        st.session_state['id_u']=user_list.index(option)
+        k=0
+        for data in st.session_state['data_list']:
+            st.download_button('Télécharger les données de '+ data.pseudo,pickle_model(data),file_name=data.pseudo+'.100cols',key='uk-1'+str(k))
+            k+=1
+
+if len(user_list)==0:
+    st.title("Liste des cols ")
+    st.warning("Pas d'utilisateur enregistré")
+else:
+    st.title("Nettoyage des cols de "+ option)
+
+if st.button('Remove space in code'):
+    st.session_state['data_list'][st.session_state['id_u']].cols.Code=st.session_state['data_list'][st.session_state['id_u']].cols.Code.str.replace(' ', '')
